f774b168581642edc5f9aabade7680a161612206
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-07-02 23:56:04 -0500

    Create Checking_account.java

---
+++ Checking_account.java
@@ -0,0 +1,8 @@
+public class CheckingView {
+    private final SimpleStringProperty Checking_account_number, Account_number, account_balance;
+
+    public CheckingView(String checking_acc_number, String account_number, String account_bal) {
+
+        
+    }
+}
