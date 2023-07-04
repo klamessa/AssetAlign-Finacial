816081f6c2818caa5de73d14fcf210436dc1609c
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-07-03 23:39:18 -0500

    Update Savings_account.java

--- Savings_account.java
+++ Savings_account.java
@@ -1,1 +1,10 @@
+public class Savings_account {
+
+    private final SimpleStringProperty Savings_account_number, Account_number, account_balance;
+
+    public Savings_account(String saving_acc_number, String acc_number, String acc_bal) {
+
+        this.Savings_account_number =new SimpleStringProperty(saving_acc_number);
+        this.Account_number =new SimpleStringProperty(acc_number);
+        this.account_balance =new SimpleStringProperty(acc_bal);
 
