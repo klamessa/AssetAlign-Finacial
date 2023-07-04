035d5f5bb455fa4ca7c7557b83e5e0114dda9d12
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-07-03 23:40:04 -0500

    Update Checking_account.java

--- Checking_account.java
+++ Checking_account.java
@@ -1,7 +1,7 @@
-public class CheckingView {
+public class Checking_account {
     private final SimpleStringProperty Checking_account_number, Account_number, account_balance;
 
-    public CheckingView(String checking_acc_number, String account_number, String account_bal) {
+    public Checking_account(String checking_acc_number, String account_number, String account_bal) {
         this.Checking_account_number =new SimpleStringProperty(checking_acc_number);
         this.Account_number =new SimpleStringProperty(account_number);
         this.account_balance =new SimpleStringProperty(account_bal);
