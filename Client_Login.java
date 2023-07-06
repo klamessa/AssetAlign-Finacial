99c15cf378cbd3b9e916d386a0cb619d07050d37
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-07-05 20:43:17 -0500

    Update Client_Login.java

--- Client_Login.java
+++ Client_Login.java
@@ -1,8 +1,2 @@
-public String getAccount_balance(){
-            return account_balance.get();
-        }
-public void setAccount_balance(String acc_number){
-        this.account_balance.set(acc_number);
-    }
-
+public class ClientLoginController {
 }
