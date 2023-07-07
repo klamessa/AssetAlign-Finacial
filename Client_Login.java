a27a1bde552d60c0a06fc97fb19095a8dec7aaf4
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-07-06 23:40:33 -0500

    Update Client_Login.java

--- Client_Login.java
+++ Client_Login.java
@@ -1,2 +1,11 @@
 public class ClientLoginController {
+protected void passwordRecovery(ActionEvent event) throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("PasswordRecovery.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxml);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
