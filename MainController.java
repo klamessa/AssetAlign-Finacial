42ae12c060df50c087d491c8241cdaf6081e71de
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-04 07:25:56 -0500

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -1,1 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainControllermodif implements Initializable {
+
+    @FXML
+    private Button LoginButton,aboutUsButton;
+    @FXML
+    private VBox vbox;
+    @FXML
+    private Parent fxml;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Translation();
+    }
+    @FXML
+    private void open_Login(ActionEvent event){
+        Translation();
+    }
+
+
+
+
+    
+
 
