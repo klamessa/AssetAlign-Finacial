bccfdaddf0c2bb923935a7dac41c946313d344a6
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-05 08:56:19 -0500

    Update PasswordRecovery.java

--- PasswordRecovery.java
+++ PasswordRecovery.java
@@ -22,3 +22,28 @@ import java.sql.SQLException;
         @FXML
         private TextField username;
 
+    public void VerifyCustomer(ActionEvent Event) throws SQLException {
+            Connectivity connectivity = new Connectivity();
+            Connection connection = connectivity.getConnection();
+            String query = "select ssn from  customer_personal_info where username = ?   and email= ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1,username.getText());
+            statement.setString(2,email.getText());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                int datassn = resultSet.getInt(1);
+                int k=0;
+                char [] social = new char[5];
+                for (int i = 4; i < 9; i++) {
+                    social[k]= String.valueOf(datassn).charAt(i);
+                    k++;
+                }
+                String  compssn= new String(social);
+                System.out.println(compssn);
+                if(compssn.equals(ssn.getText())){
+                    System.out.println("your account is  in our data base");
+                }else {
+                    System.out.println("your account is not in our data base");
+                }
+            }
+
