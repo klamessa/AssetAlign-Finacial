e098668fb0b39d3435ded0e3a71c8d50fa1928cb
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-05 08:55:56 -0500

    Update PasswordRecovery.java

--- PasswordRecovery.java
+++ PasswordRecovery.java
@@ -1,1 +1,24 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+    public class PasswordRecoveryController {
+        @FXML
+        public Button loginButton;
+        @FXML
+        private TextField ssn;
+        @FXML
+        private TextField email;
+        @FXML
+        private TextField username;
 
