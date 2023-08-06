f0fef62d30cb879bc72da0ce0c7c0fd766948f9d
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-06 12:08:15 -0500

    Update SignupPage.java

--- SignupPage.java
+++ SignupPage.java
@@ -1,1 +1,42 @@
+package com.example.demo;
+
+import com.example.demo.customer.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+
+@SuppressWarnings("ALL")
+public class SignupPageController {
+    public Label ssnLabel1;
+    public Label emailLabel1;
+    public ToggleGroup maritaltoggle;
+    public TextField UsernameF;
+    public TextField PasswordF;
+    public ComboBox state_ComboBox;
+
+    @FXML
+    private Label firstNameLabel, lastNameLabel, middleNameLabel,
+            DOBLabel, ssnLabel, phoneNumberLabel, emailLabel,
+            streetNameLabel, zipCodeLabel, aptLabel, cityLabel,
+            genderLabel, maritalStatusLabel,
+            usernameLabel, passwordLabel,
+            verifySSNLabel, verifyPhoneLabel, verifyAgeLabel,
+             SignupLabel,  progressLabel, progressPercentLabel ;
+
+
+  
 
