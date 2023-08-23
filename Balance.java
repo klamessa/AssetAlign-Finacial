63c038767e3990267f152b920d4337beb5fbca69
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-23 01:38:11 -0500

    Update Balance.java

--- Balance.java
+++ Balance.java
@@ -1,1 +1,8 @@
+package com.example.demo.customer;
+
+@SuppressWarnings("unused")
+interface Balance {
+    long withdrawal(long balance, long withdrawalAmount) throws Exception;
+    long deposit(long balance, long depositAmount) throws Exception;
+}
 
