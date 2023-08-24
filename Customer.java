b55b91f7e7b5da9e0cfe7f96deea6a073214cd2a
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-24 17:17:55 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,14 @@
+package com.example.demo.customer;
+
+import java.time.LocalDate;
+
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String phoneNumber;
+    private String email;
+    private String username;
+    private String password;
 
