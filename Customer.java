373127dea640f1dfcdb5db9837796b0465768cb6
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-08-26 23:59:09 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -99,6 +99,25 @@ public class Customer {
     public String getStreet() {
         return street;
     }
+     public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 
    
 
