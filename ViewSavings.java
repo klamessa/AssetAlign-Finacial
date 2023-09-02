22b3aa59e5ff168ba8dfa1ab70936e99d362fd82
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-02 14:02:50 -0500

    Update ViewSavings.java

--- ViewSavings.java
+++ ViewSavings.java
@@ -1,1 +1,15 @@
+package com.example.demo.Admin;
 
+import javafx.beans.property.SimpleStringProperty;
+
+public class SavingsView {
+
+    private final SimpleStringProperty Savings_account_number, Account_number, account_balance;
+
+    public SavingsView(String saving_acc_number, String acc_number, String acc_bal) {
+
+        this.Savings_account_number =new SimpleStringProperty(saving_acc_number);
+        this.Account_number =new SimpleStringProperty(acc_number);
+        this.account_balance =new SimpleStringProperty(acc_bal);
+
+}
