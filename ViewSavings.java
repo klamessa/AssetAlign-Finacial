743df806a84babaed7a0853353379fc05caf3836
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-02 14:04:18 -0500

    Update ViewSavings.java

--- ViewSavings.java
+++ ViewSavings.java
@@ -13,3 +13,29 @@ public class SavingsView {
         this.account_balance =new SimpleStringProperty(acc_bal);
 
 }
+
+public String getSavings_account_number() {
+        return Savings_account_number.get();
+    }
+
+    public void setSavings_account_number(String saving_acc_number){
+        this.Savings_account_number.set(saving_acc_number);
+    }
+
+    public String getAccount_number() {
+        return Account_number.get();
+    }
+
+    public void setAccount_number(String acc_number){
+        this.Account_number.set(acc_number);
+    }
+
+    public String getAccount_balance(){
+            return account_balance.get();
+        }
+
+    public void setAccount_balance(String acc_number){
+        this.account_balance.set(acc_number);
+    }
+
+}
