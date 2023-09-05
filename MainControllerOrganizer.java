41395bc7f12831ea8098cf175f5c407172c0892f
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-04 19:16:57 -0500

    Update MainControllerOrganizer.java

--- MainControllerOrganizer.java
+++ MainControllerOrganizer.java
@@ -1,1 +1,28 @@
+package com.example.AssetAlign;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainControllerOrganizer implements Initializable {
+
+    @FXML
+    private Button LoginButton,aboutUsButton;
+    @FXML
+    private VBox vbox;
+    @FXML
+    private Parent fxml;
