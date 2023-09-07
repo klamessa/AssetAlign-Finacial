e993e9353a20813d900bb47cf3ad43389de3f093
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-07 13:52:53 -0500

    Update MainControllerOrganizer.java

--- MainControllerOrganizer.java
+++ MainControllerOrganizer.java
@@ -62,6 +62,16 @@ public class MainControllerOrganizer implements Initializable {
             stage.centerOnScreen();
             stage.show();
         }
+          @FXML
+        protected void aboutUsPage(ActionEvent event) throws IOException {
+            fxml = FXMLLoader.load(getClass().getResource("AboutUs.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxml);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.show();
+        }
 }
 
     
