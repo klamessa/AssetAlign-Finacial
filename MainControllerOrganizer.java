e6e722d2bef0228198eb9b1931a6d1ac87441a38
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-08 13:04:39 -0500

    Update MainControllerOrganizer.java

--- MainControllerOrganizer.java
+++ MainControllerOrganizer.java
@@ -74,10 +74,15 @@ public class MainControllerOrganizer implements Initializable {
         }
         @FXML
         private void open_signup(ActionEvent event){
-             TranslateTransition transition = new TranslateTransition(Duration.seconds(1), vbox);
+            TranslateTransition transition = new TranslateTransition(Duration.seconds(1), vbox);
             transition.setToX(0);
             transition.play();
             transition.setOnFinished((e) ->{
+            try{
+                    fxml = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+                    vbox.getChildren().removeAll();
+                    vbox.getChildren().setAll(fxml);
+                }catch(IOException ex){
                     
                 }
             });
