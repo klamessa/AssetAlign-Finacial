b2357d466edd9edc9780b8d40bb6f2fa7ce98df6
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-12 11:34:51 -0500

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -2,3 +2,12 @@ package com.example.demo.Admin;
 
 import javafx.beans.property.SimpleStringProperty;
 
+public class TransactionView {
+
+    private final SimpleStringProperty Transaction_number, Account_number, Date_Created, income_date, income_deposit, income_amount, expense_date, expense_supplier, expense_amount;
+
+
+
+    }
+
+
