db8ca2d618823b398b2623719937c270690908e1
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-15 13:23:55 -0500

    Update SearchClients.java

--- SearchClients.java
+++ SearchClients.java
@@ -1,1 +1,11 @@
+package com.example.AssetAlign-Financial.Admin;
+
+public class clientSearch {
+    String Account_number, First_name, Last_name;
+
+    public clientSearch ( String Account_number,String First_name,String Last_name){
+        this.Account_number = Account_number;
+        this.First_name = First_name;
+        this.Last_name =Last_name;
+    }
 
