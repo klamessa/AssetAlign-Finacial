36ed0176719628da9591fc9dd6c9800feaa88ccc
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-09-30 14:50:41 -0500

    Update AdminDash.java

--- AdminDash.java
+++ AdminDash.java
@@ -1,4 +1,4 @@
-import com.example.demo.Connectivity;
+import com.example.AssetAlign-Financial.Connectivity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
