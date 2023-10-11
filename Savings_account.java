ecec9731c9151e8bbf26ec19e5d38f8faf70c9c1
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-10-10 23:52:55 -0500

    Update Savings_account.java

--- Savings_account.java
+++ Savings_account.java
@@ -20,8 +20,8 @@ public class Savings_account {
             return Account_number.get();
         }
         public String getAccount_balance(){
-                return account_balance.get();
-            }
+            return account_balance.get();
+        }
     
         public void setAccount_balance(String acc_number){
             this.account_balance.set(acc_number);
