3e7ec813f0ce7980cf0f3fc6c81f8b044f202209
Ketim Lamessa <99996786+klamessa@users.noreply.github.com>
2023-10-19 23:59:43 -0500

    Update BotFunction.java

--- BotFunction.java
+++ BotFunction.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scen e.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
